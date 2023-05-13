da8b90befc4b589895a7688073376baf30d3f7da
AhmadFaraazJafri <a_jafri@live.concordia.ca>
2023-05-13 08:47:15 -0500

    Added customer class

---
+++ src/main/java/org/COEN/Customer.java
@@ -0,0 +1,48 @@
+package org.COEN;
+
+public class Customer {
+    private String name;
+    private String lastName;
+    private String dateOfBirth;
+    private String email;
+
+    public Customer(String name, String lastName, String dateOfBirth, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
